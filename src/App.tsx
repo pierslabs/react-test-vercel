f3b1cc8370fedc92ed68f865695ac1b1d20a1a05
Pedro Losas <pedrolosas@MacBook-Pro-de-Pedro.local>
2024-01-22 19:47:21 +0100

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react';
-
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>hola</h1>
-    </>
-  );
+  return <h1>hola</h1>;
 }
 
 export default App;
